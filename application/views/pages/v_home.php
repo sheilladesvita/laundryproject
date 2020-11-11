4c39d10b41d59776f5f4217bb5f96b5d5b8b1071
sheilladesvita <45117449+sheilladesvita@users.noreply.github.com>
2020-11-11 23:02:09 +0700

    update v_home

--- application/views/pages/v_home.php
+++ application/views/pages/v_home.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <div class="bg-image">
 	<div class="main-banner">
 		<div class="d-block text-center">
@@ -13,6 +14,118 @@
 		>
 	</div>
 </div>
+=======
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>ALS Laundry Pickup Delivery</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" />
+    
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="jumbotron.css" rel="stylesheet">
+    <style type="text/css">
+      .logo-image{
+        width: 130px;
+        height: 50px;
+        /* overflow: hidden; */
+        margin-top: -8px;
+    }
+      h3 {
+      text-align: center;
+      font-style: italic;
+    }
+    .display-3{
+      color: white;
+      font-style:italic;
+      font-size: 3.5rem;
+      font-weight: 300;
+      line-height: 1.2;
+      font-family: Arial, Helvetica, sans-serif;
+    }
+    .img-thumb{
+      width: 200px; 
+    /* display:inline;  */
+      height: 200px;
+    }
+    .img-thumb-2{
+      float: left;
+      margin: 5px;
+    }
+    .text-right{
+      text-align: right;
+    }
+    div {
+    padding-top: 8px;
+    padding-right: 5px;
+    padding-bottom: 8px;
+    padding-left: 5px;
+    }
+    .jumbotron {
+  padding: 2rem 1rem;
+  margin-bottom: 2rem;
+  background-image: url("assets/background-1.jpg");
+  /* background-color: #cccccc; */
+  background-repeat: no-repeat;
+  /* position:fixed; */
+  width: 100%;
+  height: 350px;
+  background-size:100% 350px;
+  border-radius: 0.3rem;
+}
+  .button-1{
+    text-align: center;
+  }
+
+  #social-in:hover {
+  color: #4E433C;
+}
+
+   a{
+    margin: 10px;
+  }
+
+  .fa-instagram{
+    color: #e84393;
+  }
+  
+  .fa-whatsapp{
+    color: #25D366;
+  }
+  h5{
+    color: grey;
+  }
+  .wrapp{
+    height: 70px;
+    width: 70px;
+    background-color: #f5f6fa;
+    border-radius:50px;
+    text-align: center;
+    margin: 10px;
+    line-height: 80px;
+    box-shadow: 1px 4px 2px 2px #dcdde1;
+  }
+  .media{
+    /* height: 70vh; */
+    padding-top: 70px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+    </style>
+  </head>
+
+  <body>
+>>>>>>> Stashed changes
 
 <main role="main">
 	<div
@@ -78,6 +191,7 @@
 
 		<hr />
 
+<<<<<<< Updated upstream
 		<div class="row justify-content-center" id="slide-left-animation-4">
 			<div class="col-md-8">
 				<div
@@ -95,3 +209,43 @@
 		</div>
 	</div>
 </main>
+=======
+    <footer class="container">
+    <div class="x-column x-md x-1-3">
+      <div id="text-13" class="widget widget_text">
+        <h4 class="h-widget">Contact Us</h4>
+        <div class="textwidget">
+          ALS Laundry
+          <br>
+          Ruko Jl. Babakan Laksana No.121
+          <br>
+          (Depan Ruko d'SDL)
+          <br>
+          <br>
+          Email: 
+          </div>
+      </div>
+    <!-- </div> -->
+      <div class="media">
+      <!-- <ul> -->
+      <a class="wrapp" href="https://www.instagram.com/sheilladesvita/" target="_blank">
+      <i id="social-in" class="fa fa-instagram fa-2x social" aria-hidden="true"></i>
+      </a>
+      <a class="wrapp" href="https://api.whatsapp.com/send?phone=089526210215" target="_blank">
+      <i id="social-in" class="fa fa-whatsapp fa-2x social" aria-hidden="true"></i>
+      </a>
+      <!-- </ul> -->
+      </div>
+      <p><center>ALS LAUNDRY - ALL RIGHT RESERVED</center></p>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="assets/js/vendor/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
+>>>>>>> Stashed changes
